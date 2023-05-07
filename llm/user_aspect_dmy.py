bf4477bce2cacefe06dc2bd1a0c8f01b6f58c34e
zck <573693104@qq.com>
2023-05-07 15:45:00 +0800

    属性 情感提取分析

--- llm/user_aspect_dmy.py
+++ llm/user_aspect_dmy.py
@@ -1,39 +1,53 @@
 # 达摩院模型
 import json
+import re
 
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 import os
 
+from data_base_utils import DataBaseUtils
+
 
 
 
 class AOE:
-    def __init__(self, option, aspect):
-        self.option = option
+    def __init__(self, aspect, option, sentiment, content):
         self.aspect = aspect
+        self.option = option
+        self.sentiment = sentiment
+        self.content = content
 
 
 semantic_cls = pipeline(Tasks.siamese_uie, 'damo/nlp_structbert_siamese-aoe_chinese-base', model_revision='v1.0')
-
-# 属性情感抽取 {属性词: {情感词: None}}
-res = semantic_cls(
-    input='车上的高德地图是什么版本，能更新吗，太简单了，一不能选语音，二经常变路就容易出错，走的不是最佳路径，重新导航后又正常！定位更新太慢。用户小管家',
-    schema={
-        '属性词': {
-            '情感词': None,
-        }
-    }
-)
 aoes = []
-# res = json.dumps(res, ensure_ascii=False)
-for items in res['output']:
-    temps = []
-    for item in items:
-        type = item['type']
-        span = item['span']
-        temps.append(span)
-        if len(temps) == 2:
-            aoes.append(AOE(temps[0], temps[1]).__dict__)
-            temps.clear()
-print(json.dumps(aoes,ensure_ascii=False,indent=2))
+
+
+def deal_user_content():
+    with open('final_res.txt', 'r') as f:
+        datas = f.readlines()
+    i = len(datas)
+    for input_text in datas:
+        print(i)
+        i -= 1
+        input_text = re.sub('([^\u4e00-\u9fa5a-zA-Z0-9，。！？])', '', input_text)
+        # 属性情感抽取 {属性词: {情感词: None}}
+        res = semantic_cls(
+            input=input_text,
+            schema={
+                '属性词': {
+                    "正向": None,
+                    "负向": None,
+                    "中性": None
+                }
+            }
+        )
+        # res = json.dumps(res, ensure_ascii=False)
+        for items in res['output']:
+            # aspect, option, sentiment, content
+            aoes.append((items[0]['span'], items[1]['span'], items[1]['type'], input_text))
+
+
+deal_user_content()
+db = DataBaseUtils()
+db.insertUserViewPoint(aoes)
