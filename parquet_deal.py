aa916b32d5b41e5be3340aa7c3a5140f21afd909
zck <573693104@qq.com>
2023-06-01 11:12:42 +0800

    parquet

---
+++ parquet_deal.py
@@ -0,0 +1,4 @@
+import pandas as pd
+print(pd.read_parquet('./data/data-0a87e90d-eb84-4f7f-a7ee-61391c3ef043-0.parquet'))
+
+print(pd.read_parquet('./data/data-920addef-b5e9-485a-86f7-5ceda51785c8-0.parquet'))
